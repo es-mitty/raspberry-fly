8a133f9e0367b1fee1500de1be843c51a7fcea1d
es-mitty <esmitty1212@gmail.com>
2022-10-21 11:13:41 -0400

    Improved client program documentation.

--- flight_sim_client.py
+++ flight_sim_client.py
@@ -2,7 +2,7 @@ import pyautogui as auto
 import socketio
 import time
 
-# Globals
+# Globals for flight simulator keys
 THROTTLE_UP = 'q'
 THROTTLE_DOWN = 'a'
 THROTTLE_INCREMENT = 20
@@ -15,10 +15,12 @@ width, height = auto.size()
 print('Dimensions:', width, height)
 
 # Initialize variables
+#Track how many times we've throttled up or down
 throttle_position = 0
+#Used to terminate loop upon certain exceptions
 run = True
 
-
+#Continuously request position data from the pi
 def eventloop():
     while run:
         io.emit('collect_data')
@@ -43,21 +45,22 @@ def disconnect():
 # Get Data
 @io.event
 def flight_data(data):
+    #Reference globals
     global throttle_position
     global run
 
-    # Testing data
+    # Testing data in console
     print(data)
     print('Throttle Value:', round(data['throttle_val'] * THROTTLE_INCREMENT))
     print('Throttle Position:', throttle_position)
 
-    # Mouse
+    # Mouse position determined using potentiometer data
     x_width = round(data['x'] * width)
     y_height = round(data['y'] * height)
     try:
         auto.moveTo(x_width, y_height, duration=0.1)
 
-        # Throttle
+        # Throttle up or down based on potentionmeter data
         throttle = (round(data['throttle_val'] * THROTTLE_INCREMENT))
         if throttle > throttle_position and throttle > 2:
             auto.press(THROTTLE_UP)
